020ddfa739a7376acd028031a3db56be484b265e
Ani Khachatryan <anikhachatryan@Anis-MacBook-Pro.local>
2018-09-09 22:54:56 -0700

    lab 2

---
+++ 9.8.2018/Utilities/Utilities.java
@@ -0,0 +1,60 @@
+// Name: Ani Khachatryan
+// Date: 09/08/2018
+// Application: Utilities
+// Purpose: Demo Java classes.
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+class Utilities {
+
+   public static void main(String[] args) {
+      
+      //Demo String
+      String testStr = "Mountain";
+      int indexOfN = testStr.indexOf('n');
+      String firstStr;
+      String secondStr;
+      firstStr = "This is my first string.";
+      secondStr = " This one is my second string.";
+      firstStr = firstStr.concat(secondStr);
+      System.out.println("The index of the letter n in Mountain is: " + indexOfN + ".");
+      System.out.println("The length of the word Mountain is: " + testStr.length() + ".");
+      System.out.println("firstStr = " + firstStr);
+      
+      
+      //Demo Math (1 more method)
+      double cosOfX = Math.cos(1);
+      System.out.println("Cos of 1 is: " + cosOfX);
+      double rand = Math.random() * 75 + 1;
+      System.out.println("Some random number [0,75]: " + rand);
+      int intRand = (int) rand;
+      System.out.println("Some random number (without a fraction) is: " + intRand);
+      int absoluteInt;
+      absoluteInt = -45;
+      absoluteInt = Math.abs(absoluteInt);
+      System.out.println("The absolute value of -45 is: " + absoluteInt + ".");
+      
+      
+      //Demo Scanner
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter year: ");
+      int year = sc.nextInt();
+      System.out.println("\nYou entered: " + year);
+      System.out.println("Does the scanner have something to read? " + sc.hasNext() + ".");
+      System.out.print("Enter your age: ");
+      byte age = sc.nextByte();
+      System.out.println("\n You entered: " + age);
+      
+      
+      sc.close();
+      
+      
+      //Demo JOptionPane
+      String retVal = JOptionPane.showInputDialog(null, "Enter anything.");
+      JOptionPane.showMessageDialog(null, "You entered: " + retVal);
+      JOptionPane.showConfirmDialog(null, "Do you like ice cream?", "Choose one.", JOptionPane.YES_NO_OPTION);
+      
+   }
+   
+}
