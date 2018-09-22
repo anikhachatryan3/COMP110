f5fe17f882af5d655ae727ec9a0d9a9817848d55
Ani Khachatryan <anikhachatryan3@gmail.com>
2018-09-21 23:27:32 -0700

    more lecture 6 notes

---
+++ 9.15.2018/Lecture6/Lecture6.java
@@ -0,0 +1,17 @@
+class Lecture6 {
+   
+   public static double getCircumferenceOfACircle(double r) {
+      double c = 2 * 3.14 * r;
+      return c;
+   }
+
+   public static void main(String args[]) {
+      
+      //call method
+      double r = 2.367;
+      double results = getCircumferenceOfACircle(2.367);
+      System.out.println("Radius = " + r + " -- Circumference = " + results);
+      
+   }
+
+}
